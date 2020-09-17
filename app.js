7cb3fbe89f0040a975ddf4245cabf7ff22396944
Cris Perez <cristino.perez525@gmail.com>
2020-09-17 11:40:43 -0500

    Got the prompt functions completed, figuring out how to render properly and create output file if one doesn't exist

--- app.js
+++ app.js
@@ -9,10 +9,139 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Choice = require("inquirer/lib/objects/choice");
+const { type } = require("os");
+
+const employees = []
+
+function createTeam() {
+
+    inquirer
+        .prompt([
+
+            {
+                type: "input",
+                name: "name",
+                message: "What is the name of the employee you would like to add?"
+            },
+
+            {
+                type: "input",
+                name: "id",
+                message: "What is the ID# of this employee?"
+            },
+
+            {
+                type: "input",
+                name: "email",
+                message: "What is this employee's email address?"
+            },
+
+            {
+                type: "list",
+                name: "role",
+                message: "What kind of employee would you like to add?",
+                choices: ["Manager", "Engineer", "Intern"]
+            },
+        ]).then((answers) => {
+
+            switch (answers.role) {
+
+                case "Manager":
+
+                    inquirer
+                        .prompt([
+
+                            {
+                                type: "input",
+                                name: "officeNumber",
+                                message: "What is this manager's office number?"
+                            }
+                        ]).then((answers2) => {
+
+                            const manager = new Manager(answers.name, answers.id, answers.email, answers2.officeNumber)
+                            employees.push(manager)
+
+                            continueTeam()
+                        })
+                    break;
+
+                case "Engineer":
+
+                    inquirer
+                        .prompt([
+
+                            {
+                                type: "input",
+                                name: "github",
+                                message: "What is this engineer's github profile link?"
+                            }
+                        ]).then((answers2) => {
+
+                            const engineer = new Engineer(answers.name, answers.id, answers.email, answers2.github)
+                            employees.push(engineer)
+
+                            continueTeam()
+                        })
+                    break;
+
+                case "Intern":
+
+                    inquirer
+                        .prompt([
+
+                            {
+                                type: "input",
+                                name: "school",
+                                message: "What school does/did this intern go to?"
+                            }
+                        ]).then((answers2) => {
+
+                            const intern = new Intern(answers.name, answers.id, answers.email, answers2.school)
+                            employees.push(intern)
+
+                            continueTeam()
+                        })
+                    break;
+            }
+
+        })
+}
+
+function continueTeam() {
+
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "continue",
+                message: "Add another team member?",
+                choices: ["Yes", "No"]
+            }
+        ]).then((answers) => {
+
+            if (answers.continue === "Yes") {
+
+                createTeam()
+            } else {
+
+                console.log("Thank you for providing this team's information!")
+                console.log(employees)
+
+                if (!fs.existsSync(OUTPUT_DIR)){
+
+                    fs.mkdir(OUTPUT_DIR)
+                }
+                render(employees)
+            }
+        })
+}
+
+createTeam()
 
 
 // Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// and to create objects for each team member (using the correct classes as blueprints!
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
